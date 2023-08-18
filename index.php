74483f28edf9e5955dc517906a47f53fa8b94b82
jovahnsc <jovanshann@gmail.com>
2023-08-18 20:57:56 +0700

    Menambahkan Paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ut consequuntur impedit tempore facilis veritatis, ullam nemo corporis laboriosam harum.</p>
 </body>
 </html>
